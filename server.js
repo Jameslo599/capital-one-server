bb44561ba722e0b202794f7105bdcf8f8f6e275b
James Lo <jameslo599@gmail.com>
2024-05-08 05:38:32 -0500

    updated headers

--- server.js
+++ server.js
@@ -16,12 +16,15 @@ require("./config/passport")(passport);
 connectDB();
 
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept, authorization"
   );
-  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, DELETE"
+  );
   next();
 });
 
