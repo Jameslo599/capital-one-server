9dc3c30fbbda08de4a0041ca0e6f3e7d3c91eb3e
James Lo <jameslo599@gmail.com>
2024-05-09 07:35:40 -0500

    rolling back

--- config/database.js
+++ config/database.js
@@ -7,6 +7,7 @@ const connectDB = async () => {
     });
 
     console.log(`MongoDB Connected: ${conn.connection.host}`);
+    return conn;
   } catch (err) {
     console.error(err);
     process.exit(1);
--- server.js
+++ server.js
@@ -10,31 +10,10 @@ const accountRoutes = require("./routes/accounts");
 const cors = require("cors");
 
 require("dotenv").config({ path: "./config/.env" });
+connectDB();
 
 // Passport config
 require("./config/passport")(passport);
-
-connectDB().then(() => {
-  app.use(
-    session({
-      secret: "ewhf",
-      resave: false,
-      saveUninitialized: false,
-      store: MongoStore.create({
-        client: mongoose.connection.getClient(),
-        dbName: "capital-one",
-      }),
-      cookie: {
-        // Customize session cookie options here
-        maxAge: 3 * 24 * 60 * 60 * 1000, // Session expiration time (in milliseconds)
-        secure: true, // Set to true if serving over HTTPS
-        httpOnly: true, // Restrict access to cookies from client-side JavaScript
-        sameSite: "none", // Prevent cross-site request forgery
-        // Other cookie options...
-      },
-    })
-  );
-});
 
 const corsOptions = {
   origin: "https://codabank.netlify.app", //Your Client, do not write '*'
@@ -62,6 +41,25 @@ app.use(express.json());
 app.use(logger("dev"));
 // Sessions
 app.set("trust proxy", 1);
+app.use(
+  session({
+    secret: "ewhf",
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({
+      client: mongoose.connection.getClient(),
+      dbName: "capital-one",
+    }),
+    cookie: {
+      // Customize session cookie options here
+      maxAge: 3 * 24 * 60 * 60 * 1000, // Session expiration time (in milliseconds)
+      secure: true, // Set to true if serving over HTTPS
+      httpOnly: true, // Restrict access to cookies from client-side JavaScript
+      sameSite: "none", // Prevent cross-site request forgery
+      // Other cookie options...
+    },
+  })
+);
 
 // Passport middleware
 app.use(passport.initialize());
